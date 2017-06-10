eaef49aada73b9daeec7494ac0dfc9f460270f0b
Andrea Kanya <andrea.kanya@gmail.com>
2017-06-10 22:45:09 +0200

    invert function debug and css update:calculator background color.

--- calculator.js
+++ calculator.js
@@ -17,18 +17,24 @@ function getResult(value){
 
 
 function getPercentage(value){
+if (value === 0){
+        document.getElementById("screen").value
+    }
+    else {
     var num = document.getElementById("screen").value;
     var percentage = num /100
     document.getElementById("screen").value = percentage
+    }
 }
 
 
 function invert(value){
-    var num = document.getElementById("screen").value
-    if (num === 0) {
-        document.getElementById("screen").value = num
+    if (value === 0){
+        document.getElementById("screen").value
     }
-    else if (num > 0 || num < 0) {
-        document.getElementById("screen").value = num * (-1);
+    else {
+    var num = document.getElementById("screen").value
+    var invert = num*(-1)
+        document.getElementById("screen").value = invert
     }
 }
